f144e8ec82a8d87cd223f751ea14a0c8342d9947
Tejaswini Prakash Dindalakoppa <tejcoding004@gmail.com>
2024-12-28 11:03:18 +0530

    Add files via upload

---
+++ Day-10 Kadane's Algorithm.java
@@ -0,0 +1,12 @@
+class Solution {
+    public long maxSubarraySum(int[] arr) {
+        long maxh = 0, maxf = Long.MIN_VALUE;
+
+        for (int num : arr) {
+            maxh = Math.max(num, maxh + num);
+            maxf = Math.max(maxf, maxh);
+        }
+
+        return maxf;
+    }
+}
---
+++ Day-8 Stock Buy and Sell – Max one Transaction Allowed.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maximumProfit(int prices[]) {
+        int buyPrice = prices[0], maxProfit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > buyPrice) { 
+                maxProfit = Math.max(maxProfit, prices[i] - buyPrice); 
+            } else {
+                buyPrice = prices[i]; 
+            }
+        }
+        return maxProfit;
+    }
+}
