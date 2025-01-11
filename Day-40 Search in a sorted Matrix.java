96368c992b7950238818b37063eed7762941427e
Tejaswini Prakash Dindalakoppa <tejcoding004@gmail.com>
2025-01-11 23:36:15 +0530

    Add files via upload

---
+++ Day-40 Search in a sorted Matrix.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean searchMatrix(int[][] mat, int x) {
+        int n = mat.length, m = mat[0].length, l = 0, r = n * m - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2, val = mat[mid / m][mid % m];
+            if (val == x) return true;
+            if (val < x) l = mid + 1;
+            else r = mid - 1;
+        }
+        return false;
+    }
+}
